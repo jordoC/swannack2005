b30cda4b8cd019d3f4c5b4eadb18951f01078a99
clarke <jordan.clarke@queensu.ca>
2018-06-29 23:50:38 -0400

    added documentation to the Template class. JC

--- util/Template.m
+++ util/Template.m
@@ -1,35 +1,39 @@
-%Channel model
-%   
-%   Conceptual Notes:
-%       -See inline 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%> @file Template.m
+%> @brief File used to show an example of class description
+% ======================================================================
+%> @brief Here we have a brief description of the class.
+%
+%> And here we can put some more detailed informations about the class.
+% ======================================================================
 classdef Template
     properties
+        %> Description of a property
         tmp;
     end
     methods
+        % ======================================================================
+        %> @brief Class constructor
+        %>
+        %> More detailed description of what the constructor does.
+        %>
+        %> @param param1 Description of first parameter
+        %> @param anotherParam Description of the second parametere
+        %>
+        %> @return instance of the Template class.
+        % ======================================================================
         function obj = Template(tmp)
             obj.tmp= tmp;
         end
+        % ======================================================================
+        %> @brief Brief description of the exampleMethod1 method
+        %>
+        %> @param obj instance of the Template class.
+        %>
+        %> @return instance of the Template class.
+        % ======================================================================
         function obj = test(obj, tmp)
             obj.tmp = tmp;
         end
     end
 end
-
-%rv_len = 10000;
-%std_dev = 1;
-%ap_sta_dist = 10;
-%pl_exp = 2;
-%
-%pl = ap_sta_dist^(-pl_exp);
-%
-%inphase = (std_dev/2)*randn(rv_len,1);
-%quadrature = i*(std_dev/2)*randn(rv_len,1);
-%
-%h = (inphase+quadrature)*sqrt(pl);
-
-%Magnitude should be a Rayleigh distribution
-%hist(abs(h),20)
-%Phase should be uniform
-%hist(atan(imag(h)./real(h)),20)
+hist(atan(imag(h)./real(h)),20)
