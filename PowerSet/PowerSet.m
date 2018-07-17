db0cd98b87ef10aaed747589303fb884e6d86140
clarke <jordan.clarke@queensu.ca>
2018-07-17 12:47:17 -0400

    changed the density/size to a smaller more manageable number. changed power set to take an id in as a filtering paramter. JC

--- PowerSet/PowerSet.m
+++ PowerSet/PowerSet.m
@@ -4,7 +4,7 @@
 %
 % S is a cell array
 % P is a cell array of cell arrays
-function [ P ] = PowerSet( S, numEnt )
+function [ P ] = PowerSet( S, numEnt,rootId )
     n = numel(S);
     x = 1:n;
     %numEnt = n;
@@ -17,31 +17,88 @@ function [ P ] = PowerSet( S, numEnt )
             p_ix = p_ix + 1;
         end
     end
-    P = P_tmp;
-    %k = 1;
-    %for i = 1:numel(P_tmp)
-    %    if iscell(P_tmp{1,i})
-    %        if ~cellfun('isempty',P_tmp{1,i})
-    %            if k == 1
-    %                P = {P_tmp{1,i}};
-    %            else
-    %                P{1,k} = {P_tmp{1,i}};
-    %            end
-    %            k = k+1;
-    %        end
-    %    else
-    %        if length(P_tmp{1,i}) ~= 0
-    %            if k == 1
-    %                P = {P_tmp{1,i}};
-    %            else
-    %                P{1,k} = {P_tmp{1,i}};
-    %            end
-    %            k = k+1;
-    %        end
-    %    end
-    %end
-    %if k == 1
-    %    P = {[]};
-    %end
+
+    %This section of code gets rid of null cells
+    P= P_tmp;
+    k = 1;
+    for i = 1:numel(P_tmp)
+       if iscell(P_tmp{1,i})
+           if ~cellfun('isempty',P_tmp{1,i})
+               if k == 1
+                   P = {P_tmp{1,i}};
+               else
+                   P{1,k} = P_tmp{1,i};
+               end
+               k = k+1;
+           end
+       else
+           if length(P_tmp{1,i}) ~= 0
+               if k == 1
+                   P = {P_tmp{1,i}};
+               else
+                   P{1,k} = {P_tmp{1,i}};
+               end
+               k = k+1;
+           end
+       end
+    end
+    
+    %This section of code gets rid of SUS groups that don't
+    %contain the root STA 
+    P_tmp = P;
+    P = cell(1,1);
+    k = 1;
+    for i = 1:numel(P_tmp)
+        should_add = true;
+        if iscell(P_tmp{1,i})
+            for j = 1:length(P_tmp{1,i})
+                if P_tmp{1,i}{1,j}.id == rootId 
+                    break;
+                else
+                    if j == length(P_tmp{1,i})
+                        should_add = false;
+                    end
+                end
+            end
+        else
+            should_add = (P_tmp{1,i}.id == rootId);
+        end
+        if should_add == true
+            P{k,1} = P_tmp{i};
+            k = k+1;
+        end
+    end
+
+%     %This section of code gets rid of duplicate cells
+%     P_tmp = P';
+%     P = cell(1,1);
+%     k = 1;
+%     for i = 1:numel(P_tmp)
+%         should_add = true;
+%         for j = i+1:numel(P_tmp)
+%             if length(P_tmp{1,i}) == length(P_tmp{1,j})
+%                 if iscell(P_tmp{1,i})
+%                     for l = 1:length(P_tmp{1,j})
+%                         if P_tmp{1,j}{l,1} ~= P_tmp{1,i}{l,1}
+%                             break;
+%                         else
+%                             if l == length(P_tmp{1,j})
+%                                 should_add = false;
+%                             end
+%                         end
+%                     end
+%                 else
+%                     should_add = (P_tmp{i,1} == P_tmp{j,1});
+%                 end
+%             end
+%         end
+%         if should_add == true
+%             P{k,1} = P_tmp{i};
+%             k = k+1;
+%         end
+%     end
+%     if k == 1
+%         P = {[]};
+%     end
 end
 
--- main/main.m
+++ main/main.m
@@ -6,14 +6,13 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 addpath ../2d_pt_proc;
 addpath ../;
-addpath ../@Station;
 
-ap_loc = [2,2];     %Meters
+ap_loc = [5,5];     %Meters
 num_ap_ants = 4;
 ap_tx_power = 1;    %Watts
 
-sta_density = 1;
-wlan_length = 4;   %Meters, area is square of this value
+sta_density = .0625;
+wlan_length = 10;   %Meters, area is square of this value
 a = AccessPoint(ap_loc, ap_tx_power, 4);
 
 
@@ -24,9 +23,12 @@ for sta_loc_idx = 1:num_stas
     stas = [stas, Station(sta_locs(sta_loc_idx,:))];
 end
 
-a = a.setPotAssocStas(stas,5);
+a = a.setPotAssocStas(stas,wlan_length);
 
 a = a.genStaSusSets(.001,0,0);
+
+a = a.genStaSusGroups(2);
+%ps = PowerSet(a.susGroups,2);
 
 %Plotting for verification
 %plot(1);
