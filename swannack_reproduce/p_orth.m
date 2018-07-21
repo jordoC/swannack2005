c58008392ea6626bcc2c6fd17eac144591cb8691
clarke <jordan.clarke@queensu.ca>
2018-07-20 20:54:21 -0400

    reverted dimensionality changes. JC

--- swannack_reproduce/p_orth.m
+++ swannack_reproduce/p_orth.m
@@ -1,12 +1,11 @@
 function p_orth_lbnd = p_orth(theta, group_size, num_dim)
     %Argument Validation
     classes = {'double'};
-    group_attr = {'<=',2*num_dim};
+    group_attr = {'<=',num_dim};
     funcName = 'p_orth';
 
     validateattributes(group_size,classes,group_attr,funcName)
     l = group_size;
-    %[delta_lbnd delta_ubnd] = delta_c(theta, 2*num_dim);
-    [delta_lbnd delta_ubnd] = delta_c(theta, num_dim);
+    [delta_lbnd delta_ubnd] = delta_c(theta, 2*num_dim);
     p_orth_lbnd = (1-(l-1)*delta_lbnd)^(l-1);
 end
