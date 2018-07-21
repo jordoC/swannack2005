10fc1cbf19bfe75fd960eb27086760b5b99aefbe
clarke <jordan.clarke@queensu.ca>
2018-07-21 14:41:46 -0400

    commit of orthogonal data sets, plots, plotting script. JC

--- swannack_reproduce/widely_linear/p_orth.m
+++ swannack_reproduce/widely_linear/p_orth.m
@@ -4,7 +4,7 @@ function p_orth_lbnd = p_orth(theta, group_size, num_dim)
     group_attr = {'<=',2*num_dim};
     funcName = 'p_orth';
 
-    validateattributes(group_size,classes,group_attr,funcName)
+    %validateattributes(group_size,classes,group_attr,funcName)
     l = group_size;
     [delta_lbnd delta_ubnd] = delta_c(theta, num_dim);
     p_orth_lbnd = (1-(l-1)*delta_lbnd)^(l-1);
