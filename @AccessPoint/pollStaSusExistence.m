de6b5cd2103f92c22e05dfcdd91f63adfa0a357f
clarke <jordan.clarke@queensu.ca>
2018-08-03 17:24:47 -0400

    reworked polling method to selects SUS sets one at a time rather than as a grid-cell array. JC

--- @AccessPoint/pollStaSusExistence.m
+++ @AccessPoint/pollStaSusExistence.m
@@ -1,13 +1,8 @@
 function obj = pollStaSusSetExistence(obj, orthThresh, snrLower, snrUpper, groupSize, numStas)
-    %This is another method of guaranteeing that there will be enough STAs in the PPP to select from. However,
-    % if this min() is used, it does not guarantee that the number of STAs considered for SUS group existence is what was
-    % requested (ie. if the PPP has fewer STAs in it than the number of STAs requested for consideration)
-    
-    %num_pot_stas = min(numStas,length(obj.potAssocStas));
-
-    %Alternatively, this assignment will either guarantee that the number of STAs requsted for consideration is what the
-    %user asked for, or it will through an error.
     num_pot_stas = numStas;
+
+    S_e = {};
+    S_e_idx = 1;
 
     %We randomly select STAs from the PPP to consider for SUS group addition:
     potStas = [];
@@ -15,105 +10,78 @@ function obj = pollStaSusSetExistence(obj, orthThresh, snrLower, snrUpper, group
     for i = 1:num_pot_stas
         sta_idx = sta_idx_vec(i);
         if i == 1
-            potStas = obj.potAssocStas(sta_idx);
+            potStas = {obj.potAssocStas(sta_idx)};
         else
-            potStas = [ potStas obj.potAssocStas(sta_idx) ];
+            potStas{i} = obj.potAssocStas(sta_idx);
         end
     end
+  
+    %S_a is the collection of all sets of size groupSize, chosen from a pool of size numStas  
+    S_a = combnk(potStas,groupSize);
     
-    %SNR Requirement checking:
-    % Check to see if the STA channel is within the SNR bounds. This is done by forming the norm of the random channel
-    % accross the number of dimensions corresponding to the number of transmit channels, then randomly selecting a sample
-    % from the random variable representing the norm. It is important to note that if the STA does not meet SNR requirements,
-    % it is disqualified from SIR/orthogonality consideration. It is assumed when considering orthogonality, that the channel
-    % norm fits inbetween the shell defined by rho_min, rho_max (ie. the channel meets SNR requirements). If it does not, then
-    % it should be automatically disqualified from consideration as a candidate member of the SUS group.
-    %
-    % In terms of implementation, this is achieved by maintaining a list of STAs to exclude from SIR consideration
-    % (ie. excl_stas)
-    excl_stas = [];
-    sus_created = false;
-    for i = 1:num_pot_stas
-        stas_vec = [];
-        for j = 1:num_pot_stas
-            %Get the channel vectors to compare
-            H1 = zeros(length(potStas(i).channel(1).h),length(potStas(i).channel));
-            H2 = zeros(length(potStas(j).channel(1).h),length(potStas(j).channel));
-            for k = 1:length(H1(1,:))
-                H1(:,k) = potStas(i).channel(k).h;
-                H2(:,k) = potStas(j).channel(k).h;
+    norm_incl_stas = [];
+    for S_a_idx  = 1:length(S_a(:,1))
+        A = S_a(S_a_idx,:);
+        for A_idx = 1:length(A)
+            H = zeros(length(A{A_idx}.channel(1).h),length(A{A_idx}.channel));
+            for chan_idx = 1:length(H(1,:))
+                H(:,chan_idx) = A{A_idx}.channel(chan_idx).h;
             end
-            %If i == j, then two STAs are the same, we are forming the channel norm
-            if i == j
-                %By forming this matrix multiplication, we end up with a square matrix with a dimension equal to the
-                % number of samples in the random channel vector. Note that the non-diagonal elements are not correlated,
-                % but the elements along the main diagonal are. We randomly select a sample along the main diagonal to see
-                % if it meets SNR requirements
-                norm_val = datasample(diag(H2*ctranspose(H1)),1);
-                
-                %Note that the average of the norm samples corresponding to the channel should be 1:
-                %norm_val = abs(trace(ctranspose(H1)*H2)/length(H1(:,1))); %should be = 1
-
-                %Check SNR requirements
-                if (norm_val >= snrLower) && (norm_val<= snrUpper)
-                    if (i == 1)
-                        stas_mat = {potStas(j)};
-                        sus_created = true;
-                        %break;
+            %By forming this matrix multiplication, we end up with a square matrix with a dimension equal to the
+            % number of samples in the random channel vector. Note that the non-diagonal elements are not correlated,
+            % but the elements along the main diagonal are. We randomly select a sample along the main diagonal to see
+            % if it meets SNR requirements
+            norm_val = datasample(diag(H*ctranspose(H)),1);
+            if (norm_val >= snrLower) && (norm_val<= snrUpper)
+                if A_idx == length(A)
+                    if length(norm_incl_stas) == 0
+                        norm_incl_stas = S_a_idx;
                     else
-                        stas_mat{i,j} = potStas(j);
-                    end
-                else
-                    %If requirements are not met, then leave the cell empty and add the sta_mat index to the exclusion list
-                    if (i == 1)
-                        stas_mat = {};
-                        excl_stas = i;
-                        sus_created = true;
-                    else
-                        excl_stas = [excl_stas i];
+                        norm_incl_stas = [norm_incl_stas S_a_idx];
                     end
                 end
+            else
+                break;
             end
         end
     end
 
-    %SIR Requirement checking:
-    % Check the SIR requirements for those channels that live in the shell between rho_min, rho_max.
-    for i = 1:num_pot_stas
-        stas_vec = [];
-        for j = 1:num_pot_stas
-            %Get the channel vectors to compare
-            H1 = zeros(length(potStas(i).channel(1).h),length(potStas(i).channel));
-            H2 = zeros(length(potStas(j).channel(1).h),length(potStas(j).channel));
-            for k = 1:length(H1(1,:))
-                H1(:,k) = potStas(i).channel(k).h;
-                H2(:,k) = potStas(j).channel(k).h;
+    for S_a_idx = norm_incl_stas
+        A = S_a(S_a_idx,:);
+        orth_pairs = combnk(A,2);
+        for orth_idx = 1:length(orth_pairs(:,1))
+            orth_pair = orth_pairs(orth_idx,:);
+            H1 = zeros(length(orth_pair{1}.channel(1).h),length(orth_pair{1}.channel));
+            H2 = zeros(length(orth_pair{2}.channel(1).h),length(orth_pair{2}.channel));
+            for chan_idx  = 1:length(H1(1,:))
+                H1(:,chan_idx) = orth_pair{1}.channel(chan_idx).h;
+                H2(:,chan_idx) = orth_pair{2}.channel(chan_idx).h;
             end
-            %We have already checked the i == j case, now we want to consider the off-diagonal elements in the cell array
-            % that correspond to interference. Checking is done as a measure of orthogonality.
-            if i ~= j
-                orth_val = H2*ctranspose(H1);
-                %It really shouldn't matter if we sample along the diagonal of this matrix, since all the channels are
-                % assumed to be iid. The orth_val matrix should be a square matrix of dimension equal to the number of
-                % samples in the random vector representing the channel. Really, each row/column and the main diagonal
-                % should be statistically equivalent random variables; therefore, it shouldn't matter which vector of the
-                % matrix we choose to select a sample from.
-                %
-                %Randomly select a sample from the random variable to check if it meets the orthogonality requirement
-                orth_samp = abs(datasample(diag(orth_val),1));
-                if (orth_samp < orthThresh) && ~(any(excl_stas==j))
-                    stas_mat{i,j} = potStas(j);
-                else
-                    %If the requirement is not met, or the index is in the exclusion list from the SNR requirement checking,
-                    % then leave the cell empty.
-                    stas_mat{i,j} = [];
-                end
-            end 
+            orth_val = H2*ctranspose(H1);
+            %It really shouldn't matter if we sample along the diagonal of this matrix, since all the channels are
+            % assumed to be iid. The orth_val matrix should be a square matrix of dimension equal to the number of
+            % samples in the random vector representing the channel. Really, each row/column and the main diagonal
+            % should be statistically equivalent random variables; therefore, it shouldn't matter which vector of the
+            % matrix we choose to select a sample from.
+            %
+            %Randomly select a sample from the random variable to check if it meets the orthogonality requirement
+            orth_samp = abs(datasample(diag(orth_val),1));
+            if (orth_samp < orthThresh)
+               if orth_idx == length(orth_pairs(:,1))
+                   if length(S_e) == 0
+                       S_e = {A};
+                       S_e_idx = S_e_idx + 1;
+                   else
+                       S_e{S_e_idx} = A;
+                       S_e_idx = S_e_idx + 1;
+                   end
+               end
+            else
+                break;
+            end
         end
-    end
-    %The rows in the cell array represent potential SUS groups. The cells will either be empty (ie. containing {0xdouble}),
-    % or will contain a STA (ie. {1xStation}. When the cell array is fed into the SUS group selection method that permutes
-    % the contents of these cells, all the empty cells will be skipped over, and the non-empty cells will be permuted in
-    % order to form SUS groups of the specified group size.
-    obj.susPollSets = stas_mat;
+    end 
+    obj.susPollSets = S_e;
+    %length(norm_incl_stas)
+    %length(S_e)/length(norm_incl_stas)
 end
--- main/main.m
+++ main/main.m
@@ -14,28 +14,29 @@ ap_tx_power = 1;    %Watts %TODO: Depricated
 
 %Number of trials to run to randomly select channels from
 existence_trial_len = 50;
-existence_ctr = 0;
 
 %SNR, SIR requirements, group size, number of STAs to consider
 rho_min = 1;
 rho_max = 2;
-epsilon = .24; %TODO: seems to be off by a factor of 5
-gs = 2;%num_ap_ants;
-numStas = 13;%TODO: change back to ~30 for 90% reproduction of gs=2
+epsilon = .2; %TODO: seems to be off by a factor of 5
+gs = 3;%num_ap_ants;
+numStas = 8:2:42;%TODO: change back to ~30 for 90% reproduction of gs=2
 
-%Parameters of the PPP: 
-% we expect the number of STAs to be roughly equivalent to densitly*length^2
-wlan_length = 10;   %Meters, area is square of this value
-sta_density = numStas/(wlan_length^2); 
-
-
-for trial_idx = 1:existence_trial_len
+p_existence = zeros(1,length(numStas))
+k = 1;
+for numSta = numStas
+    existence_ctr = 0;
+    %Parameters of the PPP: 
+    % we expect the number of STAs to be roughly equivalent to densitly*length^2
+    wlan_length = 10;   %Meters, area is square of this value
+    sta_density = numSta/(wlan_length^2); 
+        
     %Create AP object, PPP representing location of STAs, and register STAs with AP
     a = AccessPoint(ap_loc, ap_tx_power, num_ap_ants);
     %Check that minimum density is met.
     while 1
         [sta_locs density] = ppp_2d(sta_density, wlan_length);
-        if (density*wlan_length^2)>= numStas
+        if (density*wlan_length^2)>= numSta
             break;
         end
     end
@@ -44,19 +45,16 @@ for trial_idx = 1:existence_trial_len
     for sta_loc_idx = 1:num_stas
         stas = [stas, Station(sta_locs(sta_loc_idx,:))];
     end
-    %TODO: move this back to the section dealing with APs, it's just here to speed up sim times. 
-    a = a.setPotAssocStas(stas,wlan_length);
-    a = a.pollStaSusExistence(epsilon,rho_min,rho_max, gs, numStas);
-    a = a.genStaSusGroups(gs);
-
-    %Check if the SUS groups have a group that meets the specified group size
-    % if it does, then an SUS gorup exists
-    for group_idx = 1:length(a.susGroups)
-        gs_len = length(a.susGroups{group_idx});
-        if gs_len >= gs
+    for trial_idx = 1:existence_trial_len
+        %TODO: move this back to the section dealing with APs, it's just here to speed up sim times. 
+        a = a.setPotAssocStas(stas,wlan_length);
+        a = a.pollStaSusExistence(epsilon,rho_min,rho_max, gs, numSta);
+        if(length(a.susPollSets)>0)
             existence_ctr = existence_ctr + 1;
-            break;
         end
     end
+    p_existence(k) = existence_ctr/existence_trial_len
+    k = k+1;
 end
-pr_existence = existence_ctr/existence_trial_len
+
+plot(numStas,p_existence)
