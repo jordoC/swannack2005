653f54d7f6920e195f81e24d2221c465d59fe654
clarke <jordan.clarke@queensu.ca>
2018-08-03 11:04:14 -0400

    intermediate commit of main and ap poll methods. neads cleanup. JC

--- @AccessPoint/pollStaSusExistence.m
+++ @AccessPoint/pollStaSusExistence.m
@@ -1,8 +1,10 @@
 function obj = pollStaSusSetExistence(obj, orthThresh, snrLower, snrUpper, groupSize, numStas)
-    num_pot_stas = min(numStas,length(obj.potAssocStas));
+    %num_pot_stas = min(numStas,length(obj.potAssocStas));
+    num_pot_stas = numStas;
     potStas = [];
+    sta_idx_vec = randperm(length(obj.potAssocStas),num_pot_stas);
     for i = 1:num_pot_stas
-        sta_idx = ceil(rand(1)*length(obj.potAssocStas));
+        sta_idx = sta_idx_vec(i);
         if i == 1
             potStas = obj.potAssocStas(sta_idx);
         else
@@ -10,19 +12,26 @@ function obj = pollStaSusSetExistence(obj, orthThresh, snrLower, snrUpper, group
         end
     end
     %num_pot_stas = 2;
-    orth_val =[];
+    excl_stas = [];
     sus_created = false;
     for i = 1:num_pot_stas
         stas_vec = [];
         for j = 1:num_pot_stas
             H1 = zeros(length(potStas(i).channel(1).h),length(potStas(i).channel));
             H2 = zeros(length(potStas(j).channel(1).h),length(potStas(j).channel));
-            for k = length(H1(1,:))
+            for k = 1:length(H1(1,:))
                 H1(:,k) = potStas(i).channel(k).h;
                 H2(:,k) = potStas(j).channel(k).h;
             end
             if i == j
-                norm_val = sum(sum(ctranspose(H1)*H2))/length(H1(:,1))
+                %H2*ctranspose(H1)
+                %abs(trace(H2*ctranspose(H1))/length(H1(:,1)))
+                norm_val = datasample(diag(H2*ctranspose(H1)),1);
+                %norm_val = abs(trace(ctranspose(H1)*H2)/length(H1(:,1)));
+                %norm_val = ((H2*ctranspose(H1))/length(H1(1,:)));
+                %(ctranspose(H1)*H2)/length(H1(:,1))
+                %(trace(ctranspose(H1)*H2))/length(H1(:,1))
+                %norm_val = sum(sum(ctranspose(H1)*H2))/length(H1(:,1))
                 if (norm_val >= snrLower) && (norm_val<= snrUpper)
                     if (i == 1)
                         stas_mat = {potStas(j)};
@@ -34,14 +43,39 @@ function obj = pollStaSusSetExistence(obj, orthThresh, snrLower, snrUpper, group
                 else
                     if (i == 1)
                         stas_mat = {};
+                        excl_stas = i;
                         sus_created = true;
+                    else
+                        excl_stas = [excl_stas i];
                     end
                 end
-            else
+            end
+        end
+    end
+    for i = 1:num_pot_stas
+        stas_vec = [];
+        for j = 1:num_pot_stas
+            H1 = zeros(length(potStas(i).channel(1).h),length(potStas(i).channel));
+            H2 = zeros(length(potStas(j).channel(1).h),length(potStas(j).channel));
+            for k = 1:length(H1(1,:))
+                H1(:,k) = potStas(i).channel(k).h;
+                H2(:,k) = potStas(j).channel(k).h;
+            end
+            if i ~= j
                 %orth_val = calcOrth(potStas(i), potStas(j)); 
-                orth_val = abs(sum(sum(ctranspose(H1)*H2)))/length(H1(:,1));
-                if orth_val < orthThresh
+                %orth_val = (H2*ctranspose(H1))/length(H1(1,:));
+                orth_val = (H2*ctranspose(H1));
+                orth_samp = abs(datasample(diag(orth_val),1));
+                %norm_val = vecnorm(orth_val)/length(H1(:,1));
+                %mean_norm_val = mean(abs(orth_val));
+                %mean_norm_val = mean(mean(abs(orth_val)));
+                %mean_norm_val = mean(norm_val) ;
+                %orth_val = abs(trace((ctranspose(H1)*H2)))/length(H1(:,1));
+                %orth_val = cov(H1,H2)
+                if (orth_samp < orthThresh) && ~(any(excl_stas==j))
                     stas_mat{i,j} = potStas(j);
+                else
+                    stas_mat{i,j} = [];
                 end
             end 
         end
--- main/main.m
+++ main/main.m
@@ -11,11 +11,10 @@ ap_loc = [5,5];     %Meters
 num_ap_ants = 4;
 ap_tx_power = 1;    %Watts
 
-sta_density = .20;
+sta_density = .5;
 wlan_length = 10;   %Meters, area is square of this value
-a = AccessPoint(ap_loc, ap_tx_power, 4);
 
-
+a = AccessPoint(ap_loc, ap_tx_power, num_ap_ants);
 sta_locs = ppp_2d(sta_density, wlan_length);
 num_stas = length(sta_locs(:,1));
 stas = [];
@@ -23,17 +22,33 @@ for sta_loc_idx = 1:num_stas
     stas = [stas, Station(sta_locs(sta_loc_idx,:))];
 end
 
-a = a.setPotAssocStas(stas,wlan_length);
 
-rho_min = 1;
-rho_max = 2;
-epsilon = .5;
-gs = 2;
-numStas = 15;
+existence_trial_len = 50;
+existence_ctr = 0;
 
-a = a.pollStaSusExistence(.001,rho_min,rho_max, gs, numStas);
+for trial_idx = 1:existence_trial_len
+    %TODO: move this back to the section dealing with APs, it's just here to speed up sim times. 
+    a = a.setPotAssocStas(stas,wlan_length);
 
-a = a.genStaSusGroups(2);
+    rho_min = 1;
+    rho_max = 2;
+    epsilon = .24; %TODO: seems to be off by a factor of 5
+    gs = 2;%num_ap_ants;
+    numStas = 13;%TODO: change back to ~30 for 90% reproduction of gs=2
+
+    a = a.pollStaSusExistence(epsilon,rho_min,rho_max, gs, numStas);
+
+    a = a.genStaSusGroups(gs);
+
+    for group_idx = 1:length(a.susGroups)
+        gs_len = length(a.susGroups{group_idx});
+        if gs_len >= gs
+            existence_ctr = existence_ctr + 1;
+            break;
+        end
+    end
+end
+pr_existence = existence_ctr/existence_trial_len
 
 %Plotting for verification
 %plot(1);
