06484eb2f1e8a503e541f5a0d8642513c97bd4f5
clarke <jordan.clarke@queensu.ca>
2018-08-17 15:56:43 -0400

    added in error bars to the simulated data and added in data series for real orthogonality with a factor of two difference in the number of antennas. JC

--- swannack_reproduce/complex/figs/sim_num_stas/plot_existence.sh
+++ swannack_reproduce/complex/figs/sim_num_stas/plot_existence.sh
@@ -8,9 +8,9 @@ set style line 1 \
     pointtype 9 pointsize 1.5
 
 set style line 2 \
-    linecolor rgb '#dd181f' \
+    linecolor rgb '#0000ff' \
     linetype 1 linewidth 2 \
-    pointtype 2 pointsize 1.5
+    pointtype 8 pointsize 1.5
 
 set style line 3 \
     linecolor rgb '#ff0000' \
@@ -23,9 +23,14 @@ set style line 4 \
     pointtype 5 pointsize 1.5
 
 set style line 5 \
-    linecolor rgb '#ff00ff' \
+    linecolor rgb '#ff0000' \
     linetype 1 linewidth 2 \
-    pointtype 2 pointsize 1.5
+    pointtype 4 pointsize 1.5
+
+set style line 6 \
+    linecolor rgb '#000000' \
+    linetype 1 linewidth 2 \
+    pointtype 6 pointsize 1.5
 
 set style line 7 \
     linecolor rgb '#000000' \
@@ -36,27 +41,41 @@ set style line 7 \
 set xrange [0 : 65]
 set yrange [-0.05 : 1.05]
 
+# Function for generating error bars:
+#   Calculation comes from Modern Mathematical Methods (1988), assumes a 
+#   90% confidence interval. Other assumptions about the independence of 
+#   the Monte Carlo runs are also assumed.
+n(x)=1.654*(x*(1-x)/50)**.5
+GPFUN_n = "n(x)=1.654*(x*(1-x)/50)**.5"
+
+
 set title "SUS group existence probability vs number of candidate users considered \n Orthogonality requirement set to 0.2"
 set datafile separator ","
-plot    "eps_0.2_gs2.csv"       using 1:2 title "GS=2, Abs"  lc rgb '#0000ff' lw 2 ps 2 pt 9
-replot  "eps_0.2_gs2_theor.csv" using 1:2 title "GS=2, Abs Calc" with linespoints linestyle 1
-replot  "eps_0.2_gs2_real.csv"  using 1:2 title "GS=2, Real" lc rgb '#0000ff' lw 2 ps 2 pt 8
+plot    "eps_0.2_gs2.csv"           using 1:2:(n($2))   with yerrorbars title "Group size = 2, Simulated complex, 90% Confidence Interval"      lc rgb '#0000ff' lw 2 ps 2 pt 9
+replot  "eps_0.2_gs2_theor.csv"     using 1:2                           title "Group size = 2, Calculated complex"                              with linespoints linestyle 1
+replot  "eps_0.2_gs2_real.csv"      using 1:2:(n($2))   with yerrorbars title   "Group size = 2, Simulated real, 90% Confidence Interval "      lc rgb '#0000ff' lw 2 ps 2 pt 8
+replot  "eps_0.2_gs2_real_theor.csv"using 1:2                           title "Group size = 2, Calculated real"                                 with linespoints linestyle 2
 
-replot  "eps_0.2_gs3.csv"       using 1:2 title "GS=3, Abs"  lc rgb '#ff0000' lw 2 ps 2 pt 5
-replot  "eps_0.2_gs3_theor.csv" using 1:2 title "GS=3, Abs Calc" with linespoints linestyle 4
-replot  "eps_0.2_gs3_real.csv"  using 1:2 title "GS=3, Real" lc rgb '#ff0000' lw 2 ps 2 pt 4
+replot  "eps_0.2_gs3.csv"           using 1:2:(n($2))   with yerrorbars title "Group size = 3, Simulated complex, 90% Confidence Interval"      lc rgb '#ff0000' lw 2 ps 2 pt 5
+replot  "eps_0.2_gs3_theor.csv"     using 1:2                           title "Group size = 3, Calculated complex"                              with linespoints linestyle 4
+replot  "eps_0.2_gs3_real.csv"      using 1:2:(n($2))   with yerrorbars title "Group size = 3, Simulated real, 90% Confidence Interval"         lc rgb '#ff0000' lw 2 ps 2 pt 4
+replot  "eps_0.2_gs3_real_theor.csv" using 1:2                          title "Group size = 3, Calculated real"                                 with linespoints linestyle 5
 
-replot  "eps_0.2_gs4.csv"       using 1:2 title "GS=4, Abs"  lc rgb '#000000' lw 2 ps 2 pt 7
-replot  "eps_0.2_gs4_theor.csv" using 1:2 title "GS=4, Abs Calc" with linespoints linestyle 7
-replot  "eps_0.2_gs4_real.csv"  using 1:2 title "GS=4, Real" lc rgb '#000000' lw 2 ps 2 pt 6
+replot  "eps_0.2_gs4.csv"           using 1:2:(n($2))   with yerrorbars title "Group size = 4, Simulated complex, 90% Confidence Interval"      lc rgb '#000000' lw 2 ps 2 pt 7
+replot  "eps_0.2_gs4_theor.csv"     using 1:2                           title "Group size = 4, Calculated complex"                              with linespoints linestyle 7
+replot  "eps_0.2_gs4_real.csv"      using 1:2:(n($2))   with yerrorbars title "Group size = 4, Simulated real, 90% Confidence Interval"         lc rgb '#000000' lw 2 ps 2 pt 6
+replot  "eps_0.2_gs4_real_theor.csv"using 1:2                           title "Group size = 4, Calculated real"                                 with linespoints linestyle 6
 
 #set key  font ",8" spacing 0 right top Left box 0
-set key  font ",8" right top 
+#set key out vert
+set key top right
+set key  font ",12" 
 set ylabel "Probability of SUS group existence" font ",15"
 set xlabel "Number of users considered" font ",15"
 set grid
 
-set term png 
+set term png size 1800, 1200
+set size 1,1
 set output "eps_0.2.png"
 
 replot
@@ -65,20 +84,25 @@ set title "SUS group existence probability vs number of candidate users consider
 set xrange [0 : 30]
 set yrange [-0.05 : 1.05]
 
-plot    "eps_0.4_gs2.csv"       using 1:2 title "GS=2, Abs"  lc rgb '#0000ff' lw 2 ps 2 pt 9
-replot  "eps_0.4_gs2_theor.csv" using 1:2 title "GS=2, Abs Calc" with linespoints linestyle 1
-replot  "eps_0.4_gs2_real.csv"  using 1:2 title "GS=2, Real" lc rgb '#0000ff' lw 2 ps 2 pt 8
+plot    "eps_0.4_gs2.csv"           using 1:2:(n($2))   with yerrorbars title "Group size = 2, Simulated complex, 90% Confidence Interval"         lc rgb '#0000ff' lw 2 ps 2 pt 9
+replot  "eps_0.4_gs2_theor.csv"     using 1:2                           title "Group size = 2, Calculated complex"                                 with linespoints linestyle 1
+replot  "eps_0.4_gs2_real.csv"      using 1:2:(n($2))   with yerrorbars title   "Group size = 2, Simulated real, 90% Confidence Interval "         lc rgb '#0000ff' lw 2 ps 2 pt 8
+replot  "eps_0.4_gs2_real_theor.csv"using 1:2                           title "Group size = 2, Calculated real"                                    with linespoints linestyle 2
+                                                                                                                                                   
+replot  "eps_0.4_gs3.csv"           using 1:2:(n($2))   with yerrorbars title "Group size = 3, Simulated complex, 90% Confidence Interval"         lc rgb '#ff0000' lw 2 ps 2 pt 5
+replot  "eps_0.4_gs3_theor.csv"     using 1:2                           title "Group size = 3, Calculated complex"                                 with linespoints linestyle 4
+replot  "eps_0.4_gs3_real.csv"      using 1:2:(n($2))   with yerrorbars title "Group size = 3, Simulated real, 90% Confidence Interval"            lc rgb '#ff0000' lw 2 ps 2 pt 4
+replot  "eps_0.4_gs3_real_theor.csv" using 1:2                          title "Group size = 3, Calculated real"                                    with linespoints linestyle 5
+                                                                                                                                                   
+replot  "eps_0.4_gs4.csv"           using 1:2:(n($2))   with yerrorbars title "Group size = 4, Simulated complex, 90% Confidence Interval"         lc rgb '#000000' lw 2 ps 2 pt 7
+replot  "eps_0.4_gs4_theor.csv"     using 1:2                           title "Group size = 4, Calculated complex"                                 with linespoints linestyle 7
+replot  "eps_0.4_gs4_real.csv"      using 1:2:(n($2))   with yerrorbars title "Group size = 4, Simulated real, 90% Confidence Interval"            lc rgb '#000000' lw 2 ps 2 pt 6
+replot  "eps_0.4_gs4_real_theor.csv"using 1:2                           title "Group size = 4, Calculated real"                                    with linespoints linestyle 6
 
-replot  "eps_0.4_gs3.csv"       using 1:2 title "GS=3, Abs"  lc rgb '#ff0000' lw 2 ps 2 pt 5
-replot  "eps_0.4_gs3_theor.csv" using 1:2 title "GS=3, Abs Calc" with linespoints linestyle 4
-replot  "eps_0.4_gs3_real.csv"  using 1:2 title "GS=3, Real" lc rgb '#ff0000' lw 2 ps 2 pt 4
+set key  font ",12" bottom right
 
-replot  "eps_0.4_gs4.csv"       using 1:2 title "GS=4, Abs"  lc rgb '#000000' lw 2 ps 2 pt 7
-replot  "eps_0.4_gs4_theor.csv" using 1:2 title "GS=4, Abs Calc" with linespoints linestyle 7
-replot  "eps_0.4_gs4_real.csv"  using 1:2 title "GS=4, Real" lc rgb '#000000' lw 2 ps 2 pt 6
-
-set key  font ",8" bottom right 
-
+set term png size 1800, 1200
+set size 1,1
 set output "eps_0.4.png"
 
 replot
