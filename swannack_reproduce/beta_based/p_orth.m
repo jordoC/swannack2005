f5763919a25371c8ba170b392489b1f4a180ce9f
clarke <jordan.clarke@queensu.ca>
2018-09-13 14:06:38 -0400

    fixed mistake in union bound. JC

--- swannack_reproduce/beta_based/p_orth.m
+++ swannack_reproduce/beta_based/p_orth.m
@@ -7,5 +7,5 @@ function p_orth_lbnd = p_orth(theta, group_size, num_dim)
     validateattributes(group_size,classes,group_attr,funcName)
     l = group_size;
     frac_area = beta_inc_reg(theta, 2*num_dim);
-    p_orth_lbnd = (max((1-(l-1)*frac_area),0))^(l-1);
+    p_orth_lbnd = (1-frac_area)^(l-1);
 end
